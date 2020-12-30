fa99ec67b726c61cbac1f67a694e80e2f5babf2c
YoungDaLee <ldy0956@gmail.com>
2020-12-30 14:14:51 +0900

    장고

--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Fcuser(models.Model):
-    username = models.CharField(max_length=32, verbose_name='사용자명')
+    username = models.CharField(max_length=64, verbose_name='사용자명')
     password = models.CharField(max_length=64, verbose_name='비밀번호')
     registered_dttm = models.DateTimeField(
         auto_now_add=True, verbose_name='등록시간')
--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,8 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from .models import Fcuser
 # Create your views here.
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        res_data = {}
+        if not (username and password):
+            res_data['error'] = '모든 값을 입력해야합니다.'
+        else:
+            fcuser = Fcuser.objects.get(username=username)
+            if check_password(password, fcuser.password):
+                # 비밀번호 일치. 로그인 처리
+                pass
+            else:
+                res_data['error'] = '비밀번호를 틀렸습니다.'
+
+        return render(request, 'login.html')
 
 
 def register(request):
