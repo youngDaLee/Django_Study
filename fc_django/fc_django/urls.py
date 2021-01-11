a20563de1e4177404c076a9292e80371f5ad06c6
YoungDaLee <ldy0956@gmail.com>
2021-01-12 02:07:42 +0900

    실전

--- fc_django/fc_django/urls.py
+++ fc_django/fc_django/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from fcuser.views import index, RegisterView, LoginView
-from product.views import ProductList, ProductCreate
+from product.views import ProductList, ProductCreate, ProductDetail
+from order.views import OrderCreate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +25,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('product/', ProductList.as_view()),
-    path('product/create/', ProductCreate.as_view())
+    path('product/create/', ProductCreate.as_view()),
+    path('product/<int:pk>/', ProductDetail.as_view()),
+    path('order/create/', OrderCreate.as_view())
 ]
--- fc_django/product/views.py
+++ fc_django/product/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
+from order.forms import RegisterForm as OrderForm
 
 # Create your views here.
 
@@ -17,3 +18,15 @@ class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterForm
     success_url = '/product/'
+
+
+class ProductDetail(DetailView):
+    template_name = 'product_detail.html'
+    queryset = Product.objects.all()
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['form'] = OrderForm()
+        return context
